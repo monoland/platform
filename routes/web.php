3b410c398adcbf1168cef199897da7ed56e9a5c6
sudarmono <30992530+monoland@users.noreply.github.com>
2024-07-16 19:35:51 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('https://' . env('SANCTUM_STATEFUL_DOMAINS'));
 });
 
 // Route::get('/{vue_capture?}', function () {
